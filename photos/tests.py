3b62da6fadd3298529ec364de86e2f918ecdddf4
Julia Agasaro <agasarojulia@gmail.com>
2019-10-14 06:21:13 +0200

    Added Tests

--- photos/tests.py
+++ photos/tests.py
@@ -2,25 +2,3 @@ from django.test import TestCase
 from .models import Location,Image,Category
 
 # Create your tests here.
-class LocationTestClass(TestCase):
-
-    # Set up method
-    def setUp(self):
-        self.kigali = Location(location = 'Kigali')
-        self.kigali.save_location()
-
-    def tearDown(self):
-        Location.objects.all().delete()
-        Category.objects.all().delete()
-   
-   
-    
-    # Testing  instance
-    def test_instance(self):
-        self.assertTrue(isinstance(self.kigali,Location))
-
-    # Testing Save Method
-    def test_save_method(self):
-        self.kigali.save_location()
-        location = Location.objects.all()
-        self.assertTrue(len(location) > 0)
