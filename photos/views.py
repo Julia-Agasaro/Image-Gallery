4985b67223413123361dfddb4519ae79e5aedda0
Julia Agasaro <agasarojulia@gmail.com>
2019-10-14 06:18:06 +0200

    Added a search function

--- photos/views.py
+++ photos/views.py
@@ -32,18 +32,7 @@ def convert_dates(dates):
     day = days[day_number]
     return day
 
-def search_results(request):
 
-    if 'image' in request.GET and request.GET["image"]:
-        search_term = request.GET.get("image")
-        searched_images = Image.search_by_image(search_term)
-        message = f"{search_term}"
-
-        return render(request, 'search.html',{"message":message,"images": searched_images})
-
-    else:
-        message = "You haven't searched for any term"
-        return render(request, 'search.html',{"message":message})
 
 def image(request, id):
     try:
